5bdc3848a96ffc6f0ffaba13633ed57551cdc82a
odai-dh <odai.dahi@hyperisland.se>
2024-11-03 17:22:35 +0100

    adding a RESTART button

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ let guestScore = 0;
 function updateScore(team, points) {
     if (team === 'home') {
         homeScore += points;
-        document.getElementById('home-score').innerText = homeScore;
+        document.getElementById('home-score').textContent = homeScore;
     } else if (team === 'guest') {
         guestScore += points;
-        document.getElementById('guest-score').innerText = guestScore;
+        document.getElementById('guest-score').textContent = guestScore;
     }
+}
+function resetScores() {
+    homeScore = 0;
+    guestScore = 0;
+    document.getElementById('home-score').innerText = homeScore;
+    document.getElementById('guest-score').innerText = guestScore;
 }
